e327bcecb0f378e55404ae8501e7bc290b7af837
Brent <brent.ellis@live.com>
2016-08-05 16:47:47 -0700

    Get Players Works

--- client/static/js/controllers/PlayerController.js
+++ client/static/js/controllers/PlayerController.js
@@ -1,19 +1,32 @@
-angular.module("PlayerController", []).controller("PlayerController", function($scope, DraftFactory){
-    $scope.drafted_players = [];
+angular.module('PlayerController', []).controller('PlayerController', function ($scope, $http, DraftFactory) {
+  $scope.drafted_players = [];
 
-    var getAll = function(){
-        DraftFactory.getAll(function(data){
-            $scope.drafted_players = data;
-        });
-    };
-    getAll();
+  $scope.get_players_api = [];
 
-    $scope.draft = function(){
-        console.log("In Client Controller Draft");
-        console.log($scope.checkboxModel1);
-        console.log($scope.checkboxModel);
-        DraftFactory.draft($scope.drafted_player, function(){
-            getAll();
-        });
-    };
-});
+  var getAll = function () {
+    DraftFactory.getAll(function (data) {
+      $scope.drafted_players = data
+    })
+  }
+  getAll()
+
+  $scope.getPlayers = function () {
+    var position = $scope.data.position_select
+    $http.get('http://www.fantasyfootballnerd.com/service/players/json/rtj7893jmh8t/' + position)
+      .success(function (data) {
+        $scope.get_players_api = angular.fromJson(data)
+      })
+      .error(function (data) {
+        console.log('Error: ' + data)
+      })
+  }
+
+  $scope.draft = function () {
+    console.log('In Client Controller Draft')
+    console.log($scope.checkboxModel1)
+    console.log($scope.checkboxModel)
+    DraftFactory.draft($scope.drafted_player, function () {
+      getAll()
+    })
+  }
+})
