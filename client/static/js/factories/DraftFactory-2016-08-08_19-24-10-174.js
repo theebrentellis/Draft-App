0a584184a27faa456d6236463f24e4dc9a3edcd9
Brent <brent.ellis@live.com>
2016-08-08 19:25:34 -0700

    Update

---
+++ client/static/js/factories/DraftFactory-2016-08-08_19-24-10-174.js
@@ -0,0 +1,32 @@
+angular.module("DraftFactory", []).factory("DraftFactory", function($http){
+    var factory = {};
+
+    var drafted = [];
+
+    factory.getAll = function(data){
+        $http.post("/getAll", data);
+    };
+
+    factory.getPlayers = function(position, callback){
+        $http.get("/getPlayers", position).success(function(output){
+            callback(output);
+        });
+    };
+
+    factory.draftPlayer = function(id, callback){
+        $http.patch("/draftPlayer/"+id).success(function(output){
+            callback();
+        });
+    };
+
+    factory.getDraftedPlayers = function(callback){
+        $http.get("/getDraftedPlayers", function(output){
+            callback(output);
+        });
+    };
+
+    factory.newDraft = function(){
+        $http.post("/newDraft");
+    };
+    return factory;
+});
