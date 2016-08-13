a28b7fde6a564d824f3a692358e605b280017f45
Brent <brent.ellis@live.com>
2016-08-13 10:30:39 -0700

    Gruntfile.js changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-nodemon');
 
   // Default task(s).
-//   grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['nodemon']);
 
 };
