66ef3515736fd918f8984a31cf3baf9dc1d2f19e
Brent <brent.ellis@live.com>
2016-08-13 10:28:17 -0700

    Gruntfile changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,15 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'server/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
+    // uglify: {
+    //   options: {
+    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //   },
+    //   build: {
+    //     src: 'server/<%= pkg.name %>.js',
+    //     dest: 'build/<%= pkg.name %>.min.js'
+    //   }
+    // },
     nodemon: {
         dev: {
             script: "server.js"
@@ -20,10 +20,10 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+//   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-nodemon');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+//   grunt.registerTask('default', ['uglify']);
 
 };
